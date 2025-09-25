da2b9b2978e9d9178bbb45c19e17ab7effb616c4
Andra Wikanjaya <andrawpt@gmail.com>
2025-09-25 19:12:27 +0700

    Minor Changes

--- quiz1/profile/script.js
+++ quiz1/profile/script.js
@@ -1,16 +1,15 @@
 const hamburger = document.querySelector(".hamburger");
 const navbar = document.querySelector(".navbar");
 
-// Toggle menu saat hamburger di-klik
 hamburger.addEventListener("click", () => {
   navbar.classList.toggle("active");
   hamburger.classList.toggle("active");
 });
 
-// Tutup menu saat salah satu link di-klik
 document.querySelectorAll(".navbar a").forEach(link => {
   link.addEventListener("click", () => {
     navbar.classList.remove("active");
     hamburger.classList.remove("active");
   });
+
 });
